dc09350375e4269b337c96de75d2e8d5f68f6f1a
b97tsk <b97tsk@gmail.com>
2019-12-29 17:39:28 +0800

    Update rangeset.go

--- rangeset.go
+++ rangeset.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"sort"
 )
 
@@ -10,94 +11,81 @@ type Range struct {
 
 type RangeSet []Range
 
-func (s *RangeSet) Add(single int64) {
-	s.AddRange(single, single+1)
+func (p *RangeSet) Add(single int64) {
+	p.AddRange(single, single+1)
 }
 
-func (s *RangeSet) AddRange(low, high int64) {
-	if low >= high {
+func (p *RangeSet) AddRange(low, high int64) {
+	s := *p
+	i := sort.Search(len(s), func(i int) bool { return s[i].Low > low })
+	j := sort.Search(len(s), func(i int) bool { return s[i].High > high })
+	if i > j {
 		return
 	}
+	if i > 0 && low <= s[i-1].High {
+		low = s[i-1].Low
+		i--
+	}
+	if j < len(s) && high >= s[j].Low {
+		high = s[j].High
+		j++
+	}
+	if i == j {
+		if low < high {
+			s = append(s, Range{})
+			copy(s[i+1:], s[i:])
+			s[i] = Range{low, high}
+			*p = s
+		}
+		return
+	}
+	s[i] = Range{low, high}
+	*p = append(s[:i+1], s[j:]...)
+}
 
-	i := sort.Search(len(*s), func(i int) bool { return (*s)[i].Low > low }) - 1
-	j := sort.Search(len(*s), func(i int) bool { return (*s)[i].High > high })
+func (p *RangeSet) Delete(single int64) {
+	p.DeleteRange(single, single+1)
+}
+
+func (p *RangeSet) DeleteRange(low, high int64) {
+	s := *p
+	i := sort.Search(len(s), func(i int) bool { return s[i].High > low })
+	t := s[i:]
+	j := i + sort.Search(len(t), func(i int) bool { return t[i].Low > high })
 	if i == j {
 		return
 	}
-
-	var r Range
-	if i >= 0 && low <= (*s)[i].High {
-		r.Low = (*s)[i].Low
-	} else {
-		r.Low = low
-		i++
-	}
-	if j < len(*s) && (*s)[j].Low <= high {
-		r.High = (*s)[j].High
-		j++
-	} else {
-		r.High = high
-	}
-
-	if i < j {
-		(*s)[i] = r
-	} else {
-		*s = append(*s, Range{})
-		copy((*s)[i+1:], (*s)[i:])
-		(*s)[i] = r
-	}
-	i++
-
-	if i < j {
-		*s = append((*s)[:i], (*s)[j:]...)
-	}
-}
-
-func (s *RangeSet) Delete(single int64) {
-	s.DeleteRange(single, single+1)
-}
-
-func (s *RangeSet) DeleteRange(low, high int64) {
-	if low >= high {
+	if i == j-1 {
+		if low > s[i].Low {
+			if high < s[i].High {
+				if low < high {
+					s = append(s, Range{})
+					copy(s[j:], s[i:])
+					s[i].High = low
+					s[j].Low = high
+					*p = s
+				}
+			} else {
+				s[i].High = low
+			}
+		} else {
+			if high < s[i].High {
+				s[i].Low = high
+			} else {
+				*p = append(s[:i], s[j:]...)
+			}
+		}
 		return
 	}
-
-	i := sort.Search(len(*s), func(i int) bool { return (*s)[i].Low > low }) - 1
-	j := sort.Search(len(*s), func(i int) bool { return (*s)[i].High > high })
-
-	var r1, r2 Range
-	if i >= 0 && low <= (*s)[i].High {
-		r1.Low, r1.High = (*s)[i].Low, low
-	} else {
+	if low > s[i].Low {
+		s[i].High = low
 		i++
 	}
-	if j < len(*s) && (*s)[j].Low <= high {
-		r2.Low, r2.High = high, (*s)[j].High
-		j++
+	if high < s[j-1].High {
+		s[j-1].Low = high
+		j--
 	}
-
-	if r1.Low < r1.High {
-		(*s)[i] = r1
-		i++
-	}
-	if r2.Low < r2.High {
-		if i < j {
-			(*s)[i] = r2
-		} else {
-			*s = append(*s, Range{})
-			copy((*s)[i+1:], (*s)[i:])
-			(*s)[i] = r2
-		}
-		i++
-	}
-
-	if i < j {
-		*s = append((*s)[:i], (*s)[j:]...)
-	}
-}
-
-func (s *RangeSet) Reset() {
-	*s = nil
+	*p = append(s[:i], s[j:]...)
 }
 
 func (s RangeSet) Contains(single int64) bool {
@@ -105,17 +93,81 @@ func (s RangeSet) Contains(single int64) bool {
 }
 
 func (s RangeSet) ContainsRange(low, high int64) bool {
-	if low >= high {
-		return false
-	}
 	i := sort.Search(len(s), func(i int) bool { return s[i].High > low })
 	return i < len(s) && s[i].Low <= low && high <= s[i].High
 }
 
 func (s RangeSet) ContainsAny(low, high int64) bool {
-	if low >= high {
+	i := sort.Search(len(s), func(i int) bool { return s[i].High > low })
+	t := s[i:]
+	return sort.Search(len(t), func(i int) bool { return t[i].Low >= high }) > 0
+}
+
+func (s RangeSet) Equals(other RangeSet) bool {
+	if len(s) != len(other) {
 		return false
 	}
-	i := sort.Search(len(s), func(i int) bool { return s[i].High > low })
-	return i < len(s) && i < sort.Search(len(s), func(i int) bool { return s[i].Low >= high })
+	for i, r := range s {
+		if r != other[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (s RangeSet) Intersect(other RangeSet) RangeSet {
+	var result RangeSet
+	for {
+		if len(s) < len(other) {
+			s, other = other, s
+		}
+		if len(other) == 0 {
+			break
+		}
+		r := other[0]
+		i := sort.Search(len(s), func(i int) bool { return s[i].High > r.Low })
+		t := s[i:]
+		j := i + sort.Search(len(t), func(i int) bool { return t[i].Low >= r.High })
+		if i < j {
+			start := len(result)
+			result = append(result, s[i:j]...)
+			if r0 := &result[start]; r0.Low < r.Low {
+				r0.Low = r.Low
+			}
+			if r1 := &result[len(result)-1]; r1.High > r.High {
+				r1.High = r.High
+			}
+			j--
+		}
+		s, other = s[j:], other[1:]
+	}
+	return result
+}
+
+func (s RangeSet) Inverse() RangeSet {
+	if len(s) == 0 {
+		return RangeSet{{math.MinInt64, math.MaxInt64}}
+	}
+	var result RangeSet
+	r0 := s[0]
+	if r0.Low > math.MinInt64 {
+		result = append(result, Range{math.MinInt64, r0.Low})
+	}
+	low := r0.High
+	for _, r := range s[1:] {
+		result = append(result, Range{low, r.Low})
+		low = r.High
+	}
+	if low < math.MaxInt64 {
+		result = append(result, Range{low, math.MaxInt64})
+	}
+	return result
+}
+
+func (s RangeSet) Sum() uint64 {
+	acc := uint64(0)
+	for _, r := range s {
+		acc += uint64(r.High - r.Low)
+	}
+	return acc
 }
--- rangeset_test.go
+++ rangeset_test.go
@@ -1,67 +1,168 @@
 package main
 
 import (
-	"fmt"
+	"math"
 	"testing"
 )
 
 func TestRangeSet(t *testing.T) {
-	var s RangeSet
-
-	assert := func(title string, ok bool) {
-		if !ok {
-			t.Fatal(title)
+	equals := func(a, b RangeSet) bool { return a.Equals(b) }
+	t.Run("TestAdd", func(t *testing.T) {
+		addRange := func(s RangeSet, r Range) RangeSet {
+			s.AddRange(r.Low, r.High)
+			return s
 		}
+		addSingle := func(s RangeSet, single int64) RangeSet {
+			s.Add(single)
+			return s
+		}
+		assert(t, "Case 1", equals(
+			addRange(RangeSet{{1, 4}, {9, 12}}, Range{5, 8}),
+			RangeSet{{1, 4}, {5, 8}, {9, 12}},
+		))
+		assert(t, "Case 2", equals(
+			addSingle(RangeSet{{1, 4}, {9, 12}}, 6),
+			RangeSet{{1, 4}, {6, 7}, {9, 12}},
+		))
+		assert(t, "Case 3", equals(
+			addRange(RangeSet{{1, 4}, {9, 12}}, Range{4, 8}),
+			RangeSet{{1, 8}, {9, 12}},
+		))
+		assert(t, "Case 4", equals(
+			addRange(RangeSet{{1, 4}, {9, 12}}, Range{5, 9}),
+			RangeSet{{1, 4}, {5, 12}},
+		))
+		assert(t, "Case 5", equals(
+			addRange(RangeSet{{1, 4}, {9, 12}}, Range{4, 9}),
+			RangeSet{{1, 12}},
+		))
+		assert(t, "Case 6", equals(
+			addSingle(RangeSet{{1, 4}, {9, 12}}, 10),
+			RangeSet{{1, 4}, {9, 12}},
+		))
+		assert(t, "Case 7", equals(
+			addRange(RangeSet{{1, 4}, {9, 12}}, Range{9, 12}),
+			RangeSet{{1, 4}, {9, 12}},
+		))
+		assert(t, "Case 8", equals(
+			addRange(RangeSet{{1, 4}, {9, 12}}, Range{12, 9}),
+			RangeSet{{1, 4}, {9, 12}},
+		))
+	})
+	t.Run("TestDelete", func(t *testing.T) {
+		deleteRange := func(s RangeSet, r Range) RangeSet {
+			s.DeleteRange(r.Low, r.High)
+			return s
+		}
+		deleteSingle := func(s RangeSet, single int64) RangeSet {
+			s.Delete(single)
+			return s
+		}
+		assert(t, "Case 1", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{7, 10}),
+			RangeSet{{1, 4}, {13, 16}},
+		))
+		assert(t, "Case 2", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{7, 9}),
+			RangeSet{{1, 4}, {9, 10}, {13, 16}},
+		))
+		assert(t, "Case 3", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{8, 10}),
+			RangeSet{{1, 4}, {7, 8}, {13, 16}},
+		))
+		assert(t, "Case 4", equals(
+			deleteSingle(RangeSet{{1, 4}, {7, 10}, {13, 16}}, 8),
+			RangeSet{{1, 4}, {7, 8}, {9, 10}, {13, 16}},
+		))
+		assert(t, "Case 5", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{1, 16}),
+			RangeSet{},
+		))
+		assert(t, "Case 6", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{1, 15}),
+			RangeSet{{15, 16}},
+		))
+		assert(t, "Case 7", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{2, 16}),
+			RangeSet{{1, 2}},
+		))
+		assert(t, "Case 8", equals(
+			deleteSingle(RangeSet{{1, 4}, {7, 10}, {13, 16}}, 5),
+			RangeSet{{1, 4}, {7, 10}, {13, 16}},
+		))
+		assert(t, "Case 9", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{4, 7}),
+			RangeSet{{1, 4}, {7, 10}, {13, 16}},
+		))
+		assert(t, "Case 10", equals(
+			deleteRange(RangeSet{{1, 4}, {7, 10}, {13, 16}}, Range{7, 4}),
+			RangeSet{{1, 4}, {7, 10}, {13, 16}},
+		))
+	})
+	t.Run("TestContains", func(t *testing.T) {
+		s := RangeSet{{1, 2}, {3, 4}}
+		assert(t, "Case 1", s.Contains(0) == false)
+		assert(t, "Case 2", s.Contains(1) == true)
+		assert(t, "Case 3", s.Contains(2) == false)
+		assert(t, "Case 4", s.Contains(3) == true)
+		assert(t, "Case 5", s.Contains(4) == false)
+		assert(t, "Case 6", s.ContainsRange(0, 2) == false)
+		assert(t, "Case 7", s.ContainsRange(1, 3) == false)
+		assert(t, "Case 8", s.ContainsRange(2, 4) == false)
+		assert(t, "Case 9", s.ContainsRange(3, 5) == false)
+		assert(t, "Case 10", s.ContainsRange(4, 6) == false)
+		assert(t, "Case 11", s.ContainsAny(0, 1) == false)
+		assert(t, "Case 12", s.ContainsAny(1, 2) == true)
+		assert(t, "Case 13", s.ContainsAny(2, 3) == false)
+		assert(t, "Case 14", s.ContainsAny(3, 4) == true)
+		assert(t, "Case 15", s.ContainsAny(4, 5) == false)
+		assert(t, "Case 16", s.ContainsAny(0, 2) == true)
+		assert(t, "Case 17", s.ContainsAny(1, 3) == true)
+		assert(t, "Case 18", s.ContainsAny(2, 4) == true)
+		assert(t, "Case 19", s.ContainsAny(3, 5) == true)
+		assert(t, "Case 20", s.ContainsAny(4, 6) == false)
+	})
+	t.Run("TestIntersect", func(t *testing.T) {
+		assert(t, "Case 1", equals(
+			RangeSet{{1, 3}}.Intersect(RangeSet{{5, 7}}),
+			RangeSet{},
+		))
+		assert(t, "Case 2", equals(
+			RangeSet{{1, 5}}.Intersect(RangeSet{{3, 7}}),
+			RangeSet{{3, 5}},
+		))
+		assert(t, "Case 3", equals(
+			RangeSet{{3, 7}}.Intersect(RangeSet{{1, 5}}),
+			RangeSet{{3, 5}},
+		))
+		assert(t, "Case 4", equals(
+			RangeSet{{2, 6}, {7, 12}}.Intersect(RangeSet{{1, 4}, {5, 9}, {10, 16}}),
+			RangeSet{{2, 4}, {5, 6}, {7, 9}, {10, 12}},
+		))
+	})
+	t.Run("TestInverse", func(t *testing.T) {
+		assert(t, "Case 1", equals(
+			RangeSet{}.Inverse(),
+			RangeSet{{math.MinInt64, math.MaxInt64}},
+		))
+		assert(t, "Case 2", equals(
+			RangeSet{{math.MinInt64, math.MaxInt64}}.Inverse(),
+			RangeSet{},
+		))
+		assert(t, "Case 3", equals(
+			RangeSet{{1, 4}, {6, 9}}.Inverse(),
+			RangeSet{{math.MinInt64, 1}, {4, 6}, {9, math.MaxInt64}},
+		))
+	})
+	t.Run("TestSum", func(t *testing.T) {
+		assert(t, "Case 1", RangeSet{}.Sum() == 0)
+		assert(t, "Case 2", RangeSet{{1, 4}}.Sum() == 3)
+		assert(t, "Case 3", RangeSet{{1, 3}, {5, 7}}.Sum() == 4)
+	})
+}
+
+func assert(t *testing.T, message string, ok bool) {
+	if !ok {
+		t.Fatal(message)
 	}
-
-	s.AddRange(0, 0)
-	assert("case 1", fmt.Sprint(s) == "[]")
-	s.Add(1)
-	assert("case 2", fmt.Sprint(s) == "[{1 2}]")
-	s.Add(0)
-	assert("case 3", fmt.Sprint(s) == "[{0 2}]")
-	s.Add(2)
-	assert("case 4", fmt.Sprint(s) == "[{0 3}]")
-	s.Add(1)
-	assert("case 5", fmt.Sprint(s) == "[{0 3}]")
-	s.AddRange(4, 7)
-	assert("case 6", fmt.Sprint(s) == "[{0 3} {4 7}]")
-	s.DeleteRange(0, 0)
-	assert("case 7", fmt.Sprint(s) == "[{0 3} {4 7}]")
-	s.DeleteRange(10, 20)
-	assert("case 8", fmt.Sprint(s) == "[{0 3} {4 7}]")
-	s.Delete(1)
-	assert("case 9", fmt.Sprint(s) == "[{0 1} {2 3} {4 7}]")
-	s.Add(1)
-	assert("case 10", fmt.Sprint(s) == "[{0 3} {4 7}]")
-	s.Delete(1)
-	s.DeleteRange(2, 6)
-	assert("case 11", fmt.Sprint(s) == "[{0 1} {6 7}]")
-	s.Reset()
-	assert("case 12", fmt.Sprint(s) == "[]")
-
-	assert("case 13", !s.ContainsRange(0, 0))
-	assert("case 14", !s.ContainsAny(0, 0))
-	s.Add(1)
-	assert("case 15", !s.Contains(0))
-	assert("case 16", s.Contains(1))
-	assert("case 17", !s.ContainsRange(0, 3))
-	assert("case 18", s.ContainsAny(0, 3))
-	s.Add(0)
-	assert("case 19", s.Contains(0))
-	assert("case 20", s.Contains(1))
-	assert("case 21", !s.ContainsRange(0, 3))
-	assert("case 22", s.ContainsAny(0, 3))
-	s.Add(2)
-	assert("case 23", s.Contains(0))
-	assert("case 24", s.Contains(1))
-	assert("case 25", s.Contains(2))
-	assert("case 26", s.ContainsRange(0, 3))
-	assert("case 27", s.ContainsAny(0, 3))
-	s.Delete(1)
-	assert("case 28", s.Contains(0))
-	assert("case 29", !s.Contains(1))
-	assert("case 30", s.Contains(2))
-	assert("case 31", !s.ContainsRange(0, 3))
-	assert("case 32", s.ContainsAny(0, 3))
 }
